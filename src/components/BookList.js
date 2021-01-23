9577c893708cecef85a66a470d80de31f7849f37
Angelika <63903851+angix-uni@users.noreply.github.com>
2021-01-23 09:21:43 +0100

    Update BookList.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -6,28 +6,20 @@ const BookList = props => (
     className="accordion md-accordion"
     id="accordionEx1"
     role="tablist"
-    aria-multiselectable="true"
   >
     <div className="card">
       <div className="card-header" role="tab" id="headingTwo1">
       <a
-          className="collapsed"
-          data-toggle="collapse"
           data-parent="#accordionEx1"
-          href="#collapseTwo1"
           aria-expanded="false"
-          aria-controls="collapseTwo1"
         >
           <h5 className="mb-0">
             {props.bookItem.volumeInfo.title}
-            <i className="fas fa-angle-down rotate-icon" />
           </h5>
         </a>
       </div>
 
       <div
-        id="collapseTwo1"
-        className="collapse"
         role="tabpanel"
         aria-labelledby="headingTwo1"
         data-parent="#accordionEx1"
